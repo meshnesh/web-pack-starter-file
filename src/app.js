c8d044f76d4152f9882a43ed7ee0f92876119f18
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-06-18 03:22:48 +0300

    Created the file structure of the project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const foundationCss = require("./foundation.min.scss");
+const mainCss = require("./app.scss");
+const jquery = require("./js/vendor/jquery.js");
+const foundationJs = require("./js/vendor/foundation.js");
+const foundationInput = require("./js/vendor/what-input.js");
+
+// console.log(css);
