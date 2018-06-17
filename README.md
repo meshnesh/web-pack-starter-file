f730122ede175955a44b8057b8959e24e6943c19
meshnesh <tonnie.nesh@gmail.com>
2018-06-17 14:30:31 +0300

    [Removed cached items]

--- README.md
+++
@@ -1,2 +0,0 @@
-# web-pack-starter-file
-A starter pack for front-end development, contains all the foundation 6 css and js files. all are linked to the index file in the dist file
--- src/app.js
+++
@@ -1,7 +0,0 @@
-const foundationCss = require("./foundation.min.scss");
-const mainCss = require("./app.scss");
-const jquery = require("./js/vendor/jquery.js");
-const foundationJs = require("./js/vendor/foundation.js");
-const foundationInput = require("./js/vendor/what-input.js");
-
-// console.log(css);
